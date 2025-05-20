57c937142d2fc550340671202bb77828d57d4f61
Arjan E <67170142+ingensargens@users.noreply.github.com>
2025-05-20 09:57:58 -0700

    Update printsort.asm

--- printsort.asm
+++ printsort.asm
@@ -37,20 +37,72 @@ endwhilereadstring:
 	li a7, 4
 	la a0, arr
 	ecall
-	j readstring
+	#j readstring
+	
 sortalpha:
-#def bubbleSort(A: list) -> list:  
- #   n = len(A)  
-  #  for i in range(n):  
-   #     swapped = False  
-    #    for j in range(0, n-i-1):  
-     #       if A[j] > A[j+1]:  
-      #          A[j], A[j+1] = A[j+1], A[j]  
-       #         swapped = True  
-        #if not swapped: break  
-    # return A
-     
+    li   t0, 0       #var pass = 0
+    la   s0, arr       # s0 = base ptr of arr
+    mv   s2, s2 # is this redundant lol
+
+outer_for:
+    bgeu t0, s2, endouter_for   # if pass >= length, done
+    lb   a0, 0(s0)     # load arr[0] into t3
+    #li   a7, 11        # print char
+    #ecall
+	
+	
+    # beginning of inner for	
+    li t1, 0 # i = 0
+    sub s3, s2, t0  #  n - 1 - pass
+	
+inner_for:
+
+        bgeu t1, s3, endouter_for # i < n - 1 - pass
+	
+	lb t2, 0(s0)
+	lb t3, 1(s0)
+	bgeu t2, t3, swap # arr[i] > arr[i+1]
+	
+	sb t2, 1(s0)
+	sb t3, 0(s0)
+	j repeat_outer_for
+	
+swap:
+mv t4, t2
+mv t2, t3
+mv t3, t4
+j repeat_outer_for
+
+
+endinner_for:
+addi t1, t1, 1
+j inner_for
+
+repeat_outer_for:
+    addi s0, s0, 1     # advance pointer of arr by one byte (ie index ++)
+    addi t0, t0, 1     # for loop ctr
+    j    outer_for        # repeat
+
+endouter_for:
+j printalpha
+
+
+
 printalpha:
+    li   t0, 0       
+    la   s0, arr      
+    mv   s2, s2 
+
+printfor:
+    bgeu t0, s2, exit   # if pass >= length, done
+    lb   a0, 0(s0)     # load arr[0] into t3
+    li   a7, 11        # print char
+    ecall
+
+endprint_for:
+    addi s0, s0, 1     # advance pointer of arr by one byte (ie index ++)
+    addi t0, t0, 1     # for loop ctr
+    j printfor
 
 exit:
     #la   t0, arr
@@ -69,11 +121,10 @@ ecall
 
 .data
 arr: .space 20
-sortedArr: .space 20
 entertext: .string "\nEnter word: "
 originaltext: .string "Original word: "
 alphawordtext: .string "\n Alphabetized word: "
-exitingtext: .string "Exiting"
+exitingtext: .string "\nExiting"
 
 
 # Bubble Sort in C
